8ef7f2f867020fdfe4405562fd19daa37de7ed7f
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-02-08 11:22:59 +0530

    Conversion And Operation

--- js-basics/conversionOperation.js
+++ js-basics/conversionOperation.js
@@ -29,3 +29,40 @@ let someNumber = 33
 let stringNumber = String(someNumber)
 console.log(typeof(stringNumber));
 console.log(stringNumber);
+
+// ********************* Operations **********************
+
+let value = 3
+let negValue = -value
+
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " Gopal"
+
+// console.log(str1 + str2);
+
+// console.log(1 + "2");
+// console.log("1" + 2);
+// console.log("1" + 2 + 2);
+// console.log(1 + 2  + "2");
+// console.log(1 + "2" + 2);
+
+// console.log(( 3 + 3 ) * 4 % 3);
+
+// console.log(+true);
+// console.log(true+);
+// console.log(+"");
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+gameCounter++;
+console.log(gameCounter);
