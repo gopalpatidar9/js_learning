61a37935b297f5672335347b78577c62e73bd8a8
gopalpatidar9 <gopalpatidara9@gmail.com>
2025-03-02 10:58:45 +0530

    Scopes

--- 03-js-basics/02_scopes.js
+++ 03-js-basics/02_scopes.js
@@ -1,3 +1,5 @@
+// scope
+
 // var c = 300
 
 let a = 300
@@ -12,3 +14,44 @@ if(true){
 console.log("global a", a);
 // console.log(b);
 // console.log(c);
+
+
+function one() {
+    const username = "Gopal"
+
+    function two() {
+        const website = "Youtube"
+        console.log("username is", username, "website is", website);
+    }
+  // console.log(website); // there we cant access website variable
+                         // out fi the scope 
+   two() 
+}
+ 
+one()
+
+if(true){
+    const username = "Virat"
+    if(username == "Virat"){
+        const website = " Youtube"
+        console.log(username + website);
+    }
+    // console.log(website);  show error becouse we cant access outof
+                              // scope
+}
+// console.log(username);   there is also outof scope show error
+
+// ++++++++++++++++++++ intersting +++++++++++++++++++++++++++
+
+console.log(addOne(5)); // there we can call the fuction before
+                        // initilization
+function addOne(num) {
+    return num + 1
+}
+console.log(addOne(5)); 
+
+// addTwo(5)  // there show error Cannot access 'addTwo' before initialization
+const addTwo = function(num){
+    return num + 2
+}
+console.log(addTwo(5))
